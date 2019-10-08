5751c4b351aa2aae0aaeb74af52156891345495b
Kevin I Rivera <kevin.ivan05@gmail.com>
2019-10-07 17:33:08 -0700

    Styling changes
    Changed css variable name to more easily primary color. which is used in buttons and step number background. Also replaced _ with () to appease linter.

--- src/TinyStep.js
+++ src/TinyStep.js
@@ -51,7 +51,7 @@ export class TinyStep extends LitElement {
                 }
 
                 :host([active]) .step-number, :host([save]) .step-number {
-                    background-color: var(--step-circle-background-color, #2196F3);
+                    background-color: var(--stepper-theme-primary, #2196F3);
                 }
 
                 .step-icon {
@@ -132,7 +132,7 @@ export class TinyStep extends LitElement {
                 }
 
                 .continue-btn, .update-btn, .finish-btn {
-                    --mdc-theme-primary: var(--submit-button-background-color, #2196F3);
+                    --mdc-theme-primary: var(--stepper-theme-primary, #2196F3);
                     --mdc-theme-on-primary: var(--submit-button-color, #FFFFFF);
                 }
 
@@ -202,12 +202,12 @@ export class TinyStep extends LitElement {
                 <slot></slot>
             </div>
             <div ?hidden="${this.hideButtons}" class="buttons">
-                <mwc-button ?hidden="${!this.save || !this.editable}" @click="${_ => this._fireEvent('update')}" class="update-btn" unelevated>${this.updateButtonText ? this.updateButtonText : 'update'}</mwc-button>
-                <mwc-button ?hidden="${this.laststep || this.save}" @click="${_ => this._fireEvent('continue')}" class="continue-btn" unelevated>${this.continueButtonText ? this.continueButtonText : 'continue'}</mwc-button>
-                <mwc-button ?hidden="${!this.optional || this.save}" @click="${_ => this._fireEvent('skip')}" class="skip-btn">${this.skipButtonText ? this.skipButtonText : 'skip'}</mwc-button>
-                <mwc-button ?hidden="${!this.laststep}"  @click="${_ => this._fireEvent('finish')}" class="finish-btn" unelevated>${this.finishButtonText ? this.finishButtonText : 'finish'}</mwc-button>
+                <mwc-button ?hidden="${!this.save || !this.editable}" @click="${() => this._fireEvent('update')}" class="update-btn" unelevated>${this.updateButtonText ? this.updateButtonText : 'update'}</mwc-button>
+                <mwc-button ?hidden="${this.laststep || this.save}" @click="${() => this._fireEvent('continue')}" class="continue-btn" unelevated>${this.continueButtonText ? this.continueButtonText : 'continue'}</mwc-button>
+                <mwc-button ?hidden="${!this.optional || this.save}" @click="${() => this._fireEvent('skip')}" class="skip-btn">${this.skipButtonText ? this.skipButtonText : 'skip'}</mwc-button>
+                <mwc-button ?hidden="${!this.laststep}"  @click="${() => this._fireEvent('finish')}" class="finish-btn" unelevated>${this.finishButtonText ? this.finishButtonText : 'finish'}</mwc-button>
                 <mwc-button @click="${_ => this._fireEvent('reset')}" class="reset-btn">${this.resetButtonText ? this.resetButtonText : 'reset'}</mwc-button>
-                <mwc-button ?hidden="${this.nonLinear || this._stepIndex === 0 }"  @click="${_ => this._fireEvent('back')}" class="back-btn">${this.backButtonText ? this.backButtonText : 'back'}</mwc-button>
+                <mwc-button ?hidden="${this.nonLinear || this._stepIndex === 0 }"  @click="${() => this._fireEvent('back')}" class="back-btn">${this.backButtonText ? this.backButtonText : 'back'}</mwc-button>
             </div>
 
         `;
